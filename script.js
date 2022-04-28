d94d2722308037c781902b62e1e9918128c1a56b
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-04-28 16:07:23 +0200

    jsnack 2 fatto

--- script.js
+++ script.js
@@ -22,47 +22,47 @@ const oggetti = [
   {
     varieta: 3,
     peso: 4,
-    lunghezza: 7
+    lunghezza: 9
   },
   {
     varieta: 3,
     peso: 6,
-    lunghezza: 7
+    lunghezza: 30
   },
   {
     varieta: 3,
     peso: 9,
-    lunghezza: 7
+    lunghezza: 13
   },
   {
     varieta: 3,
     peso: 11,
-    lunghezza: 7
+    lunghezza: 10
   },
   {
     varieta: 3,
     peso: 5,
-    lunghezza: 7
+    lunghezza: 14
   },
   {
     varieta: 3,
     peso: 4,
-    lunghezza: 7
+    lunghezza: 16
   },
   {
     varieta: 3,
     peso: 6,
-    lunghezza: 7
+    lunghezza: 20
   },
   {
     varieta: 3,
     peso: 9,
-    lunghezza: 7
+    lunghezza: 23
   },
   {
     varieta: 3,
     peso: 11,
-    lunghezza: 7
+    lunghezza: 6
   },
 
 ];
@@ -74,8 +74,36 @@ function calcoloPeso(calc){
   let somma = 0;
 
   for (const key in calc){
-    somma += calc[key].lunghezza;
+    somma += calc[key].peso;
   }
-document.getElementById('quantezucchine').innerHTML = somma;
+document.getElementById('jsnack1').innerHTML = somma;
 }
 
+/*
+
+Jsnack 2
+Crea 10 oggetti che rappresentano una zucchina.
+Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+*/
+
+dividiLunghezza(oggetti);
+
+function dividiLunghezza(lung){
+
+  let magg = 0;
+  let min = 0;
+
+  for (const key in lung) {
+
+    if ( lung[key].lunghezza > 15){
+      magg += lung[key].lunghezza;
+    } else{
+      min += lung[key].lunghezza;
+    }
+    
+  }
+  document.getElementById('maggiori15').innerHTML = magg;
+  document.getElementById('minori15').innerHTML = min;
+
+}
